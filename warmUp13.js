9fefbc89c2c153d0992082f166468abcd9dd3948
Essam-Harous <esamfarg91@gmail.com>
2019-11-01 20:49:07 +0100

    best warmUp for me

--- warmUp13.js
+++ warmUp13.js
@@ -1,6 +1,7 @@
 /* 1. Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
 
-        Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+        Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s.
+         So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
 
         Example:
 
@@ -31,3 +32,42 @@
             filter_list([1,'a','b',0,15]) == [1,0,15]
             filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 */
+
+//1
+function solution(romanNum) {
+	var obj = {
+		I: 1,
+		V: 5,
+		X: 10,
+		L: 50,
+		C: 100,
+		D: 500,
+		M: 1000
+	}
+
+	var result = 0;
+	for(var i = 0; i < romanNum.length; i++) {
+		result += obj[romanNum[i]]
+	}
+	return result;
+}
+
+//2
+function toCamelCase(str) {
+	var strA = str.split('')
+	while(strA.indexOf('-') != -1 || strA.indexOf('_') != -1){
+		if(strA.indexOf('-') != -1){
+			strA.splice(strA.indexOf('-'), 2, strA[strA.indexOf('-') + 1].toUpperCase())
+		}
+		if(strA.indexOf('_') != -1) {
+			strA.splice(strA.indexOf('_'), 2, strA[strA.indexOf('_') + 1].toUpperCase())
+		}
+	}
+    return strA.join('')
+
+}
+
+//3
+function filterStr(arr){
+	return arr.filter( element => typeof element != 'string')
+}
