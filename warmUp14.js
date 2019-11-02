8b8119d40ea14735b841c9991b924d21fe5a97fd
Essam-Harous <esamfarg91@gmail.com>
2019-11-02 20:39:11 +0100

    good warmUp

--- warmUp14.js
+++ warmUp14.js
@@ -19,5 +19,12 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 
 var maxProfit = function(prices) {
-  // YOUR CODE HERE
-};
+  // YOUR CODE HER
+  var minPrice = prices.reduce((acc, currentVal) => currentVal < acc? currentVal: acc)
+  prices.splice(0, prices.indexOf(minPrice) + 1)
+  if(prices.length === 0){
+  	return 0
+  }
+  var maxPrice = prices.reduce((acc, currentVal) => currentVal > acc? currentVal: acc)
+  return maxPrice - minPrice
+	};
