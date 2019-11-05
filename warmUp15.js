0519cbc8996ef1dedb4839e561859561780a333b
essam <esamfarg91@gmail.com>
2019-11-05 20:53:26 +0100

    bad warmup

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,43 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+function reverseStr(str){
+	var obj = {
+		word1: '',
+		word2: '',
+		word3: '',
+		word4: '',
+		word5: '',
+		word6: '',
+		word7: ''
+	}
+	var count = 1;
+	for (var i = 0; i < str.length; i++) {
+		if(str[i] === ' '){
+			count++
+		}
+		if(count == 1){
+			obj.word1 += str[i]
+		}
+		if(count == 2){
+			obj.word2 += str[i]
+		}
+		if(count == 3){
+			obj.word3 += str[i]
+		}
+		if (count == 4) {
+			obj.word4 += str[i]
+		}
+		if(count == 5) {
+			obj.word5 += str[i]
+		}
+		if(count == 6) {
+			obj.word6 += str[i]
+		}
+		if (count == 7) {
+			obj.word7 += str[i]
+		}
+	}
+	return obj.word7 obj.word6 obj.word5 obj.word4 obj.word3 obj.word2 obj.word1;
+}
